7c8db5ba6e3fde577289c82483a32616df854330
VieiraLucasDuarte <108312353+VieiraLucasDuarte@users.noreply.github.com>
2024-12-03 22:40:14 +0000

    Finalizado

--- microservcursomestrado/obj/Debug/net8.0/microservcursomestrado.AssemblyInfo.cs
+++ microservcursomestrado/obj/Debug/net8.0/microservcursomestrado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("microservcursomestrado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01784a7302a6449c4772b17f239b96040a285bf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c86aec9b5d6c8eb7e7825edda07459ef09115a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("microservcursomestrado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("microservcursomestrado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
